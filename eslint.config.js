e247debdd821390b63e4b03945e78646f6a00ce0
liting <luz.liting@gmail.com>
2023-11-20 23:37:48 +0800

    fix(vscode): enable vscode at vscode

--- eslint.config.js
+++ eslint.config.js
@@ -1,8 +1,12 @@
+// @ts-check
 import antfu from '@antfu/eslint-config'
+
+// @ts-check
 import { FlatCompat } from '@eslint/eslintrc'
 
 const compat = new FlatCompat()
 
-export default await antfu(
+// @ts-expect-error type error
+export default await antfu({
   ...compat.extends('plugin:tailwindcss/recommended'),
-)
+})
