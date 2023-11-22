eeccfeec7ebcc7d9117563d968a19a4bfad6630a
liting <luz.liting@gmail.com>
2023-11-22 22:58:30 +0800

    feat(auth): add middleware

---
+++ src/middleware.ts
@@ -0,0 +1,12 @@
+import { authMiddleware, redirectToSignIn } from '@clerk/nextjs'
+
+export default authMiddleware({
+  afterAuth(auth, req) {
+    if (!auth.userId && !auth.isPublicRoute)
+      return redirectToSignIn({ returnBackUrl: req.url })
+  },
+})
+
+export const config = {
+  matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
+}
