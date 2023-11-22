8a51c39f829bda633cb052dce8917327ff0f761d
liting <luz.liting@gmail.com>
2023-11-22 23:54:44 +0800

    feat(theme): support toggle theme for clerk components

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,4 @@
 import type { Metadata } from 'next'
-import { ClerkProvider } from '@clerk/nextjs'
 import { Inter } from 'next/font/google'
 import { Providers } from './providers'
 import './globals.css'
@@ -17,16 +16,14 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <ClerkProvider>
-      <html lang="en">
-        <body className={inter.className}>
-          <Providers>
-            <main className="h-screen w-screen overflow-auto text-foreground bg-background p-8">
-              {children}
-            </main>
-          </Providers>
-        </body>
-      </html>
-    </ClerkProvider>
+    <html lang="en">
+      <body className={inter.className}>
+        <Providers>
+          <main className="h-screen w-screen overflow-auto text-foreground bg-background p-8">
+            {children}
+          </main>
+        </Providers>
+      </body>
+    </html>
   )
 }
