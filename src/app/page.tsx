019c0e1f054cb3af0f10a9a5be3d7adf654b8fae
liting <luz.liting@gmail.com>
2023-11-23 00:05:42 +0800

    feat: add user button at / page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,14 +2,15 @@ import { Card, CardBody, CardFooter } from '@nextui-org/card'
 import { Image } from '@nextui-org/image'
 import NextImage from 'next/image'
 import Link from 'next/link'
+import { UserButton } from '@clerk/nextjs'
 import { apps } from './apps'
 
 export default function Home() {
   return (
-    <div>
+    <div className="flex gap-6">
       {apps.map(app => (
         <Link key={app.path} href={app.path}>
-          <Card className="w-24" isHoverable isPressable isBlurred>
+          <Card className="w-24" shadow="none" isHoverable isPressable isBlurred>
             <CardBody className="flex justify-center items-center">
               <Image as={NextImage} src={app.iconPath} alt={app.name} width={32} height={32} isZoomed />
             </CardBody>
@@ -19,6 +20,14 @@ export default function Home() {
           </Card>
         </Link>
       ))}
+      <Card className="w-24" shadow="none" isHoverable isPressable isBlurred>
+        <CardBody className="flex justify-center items-center">
+          <UserButton />
+        </CardBody>
+        <CardFooter className="flex justify-center items-center pt-0">
+          <span>Account</span>
+        </CardFooter>
+      </Card>
     </div>
   )
 }
