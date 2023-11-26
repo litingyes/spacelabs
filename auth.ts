ae667ed1c09299e18ffafc920b30527e6cb78a1c
liting <luz.liting@gmail.com>
2023-11-26 18:13:11 +0800

    chore(auth): add discord

--- auth.ts
+++ auth.ts
@@ -1,9 +1,11 @@
 import NextAuth from 'next-auth'
+import Discord from 'next-auth/providers/discord'
 import GitHub from 'next-auth/providers/github'
 import Google from 'next-auth/providers/google'
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [
+    Discord,
     GitHub,
     Google,
   ],
