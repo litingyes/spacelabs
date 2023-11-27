dea8267928c0c10990617525e6eb6b7c0e86086c
liting <luz.liting@gmail.com>
2023-11-28 00:33:39 +0800

    perf: adjust icon in home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,31 @@
 import { Card, CardBody, CardFooter } from '@nextui-org/card'
 import { Avatar } from '@nextui-org/avatar'
 import Link from 'next/link'
-import { apps } from './apps'
+import { Cog6ToothIcon } from '@heroicons/react/24/outline'
 import { auth } from '@/auth'
+
+const list = [
+  {
+    label: 'Setting',
+    description: 'setting for space labs',
+    path: '/setting',
+    icon: <Cog6ToothIcon className="w-10 h-10" />,
+  },
+]
 
 export default async function Home() {
   const session = await auth()
 
   return (
     <div className="flex gap-6">
-      {apps.map(app => (
-        <Link key={app.path} href={app.path}>
+      {list.map(item => (
+        <Link key={item.path} href={item.path}>
           <Card as="div" className="w-24" shadow="none" isHoverable isPressable isBlurred>
             <CardBody className="flex justify-center items-center">
-              <Avatar className="bg-transparent" src={app.iconPath} name={app.name} />
+              {item.icon}
             </CardBody>
             <CardFooter className="flex justify-center items-center pt-0">
-              <span>{app.name}</span>
+              <span>{item.label}</span>
             </CardFooter>
           </Card>
         </Link>
