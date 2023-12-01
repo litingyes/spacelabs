9d1eb9c7035c79471b166e5d84f80a3898cfeb3f
liting <luz.liting@gmail.com>
2023-12-01 23:38:03 +0800

    feat: add global breadcrumb for setting module

--- app/setting/layout.tsx
+++ app/setting/layout.tsx
@@ -3,6 +3,7 @@ import { Card } from '@nextui-org/card'
 import { Listbox, ListboxItem } from '@nextui-org/listbox'
 import { usePathname } from 'next/navigation'
 import { Cog6ToothIcon, UserIcon } from '@heroicons/react/24/outline'
+import Breadcrumb from '@/components/Breadcrumb'
 
 const list = [
   {
@@ -27,23 +28,27 @@ export default function SettingLayout({
   const pathname = usePathname()
 
   return (
-    <div className="flex h-full gap-6">
-      <Card className="w-64 overflow-auto p-2" shadow="sm" isHoverable>
-        <Listbox
-          aria-label="settings"
-        >
-          {
+    <div className="h-full flex flex-col gap-2">
+      <Breadcrumb />
+      <div className="flex flex-1 gap-6">
+        <Card className="w-64 overflow-auto p-2" shadow="sm" isHoverable>
+          <Listbox
+            aria-label="settings"
+          >
+            {
           list.map(item => (
             <ListboxItem key={item.key} href={item.url} startContent={item.icon} className={pathname.includes(item.url) ? '!bg-blue-500 !text-white' : ''}>
               {item.label}
             </ListboxItem>
           ))
         }
-        </Listbox>
-      </Card>
-      <Card className="flex-1 overflow-auto px-4 py-2" shadow="lg" isHoverable>
-        {children}
-      </Card>
+          </Listbox>
+        </Card>
+        <Card className="flex-1 overflow-auto px-4 py-2" shadow="lg" isHoverable>
+          {children}
+        </Card>
+      </div>
     </div>
+
   )
 }
